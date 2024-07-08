ffa83cf7f08e673d2b4dbaef32684106f73c3c86
gustavoMoraiss <gustavo.morais100398@gmail.com>
2024-07-08 12:29:51 -0300

    config hilt and Timber

--- app/src/main/java/com/training/interviewtechnicaltest/MainActivity.kt
+++ app/src/main/java/com/training/interviewtechnicaltest/MainActivity.kt
@@ -13,7 +13,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.training.interviewtechnicaltest.ui.theme.InterviewTechnicalTestTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
